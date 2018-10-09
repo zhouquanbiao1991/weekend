53c95e23918195e0d691a08b36cd9135e7958f60
zealotek <249764788@qq.com>
2018-10-09 14:16:40 +0800

    no

--- saturday/saturday/saturday.py
+++ saturday/saturday/saturday.py
@@ -1,14 +1,32 @@
 import graphene
+import datetime
 
 class Query_obj(graphene.ObjectType):
-    hell = graphene.String(name = graphene.String(default_value="stranger"))
+   # normal_val = ' who r u '
+    first = graphene.String()
 
-    def resolve_hell(self, info, name):
-        return "ni hao" + name
+    sec = graphene.String(sec_val = graphene.String(default_value="stranger"))
+    
+    third_val = graphene.String(default_value="stranger")    
+    third = graphene.String(third_val)
+
+    hell = graphene.String()
+    episode = graphene.Enum('episode', [('a',1), ('b',2), ('c',3)])
+    
+    def resolve_first(self, info):
+        return "first"
+    def resolve_sec(self, info, sec_val):
+        return "sec" + sec_val
+    def resolve_third(self, info, sec_val):
+        return "third" + sec_val
 
 schema = graphene.Schema(query=Query_obj)
-result = schema.execute("{hell}")
-print(result.data["hell"])
+result1 = schema.execute("{first}")
+#result2 = schema.execute("{sec}")
+#result3 = schema.execute("{third}")
+print(result.data["first"])
+#print(result.data["sec"])
+#print(result.data["third"])
 
 
 
